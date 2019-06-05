3f44a5add71406e72514a34e1895049b54c1f522
wishl <18518137760@163.com>
2019-06-05 14:52:26 +0800

    导入

--- src/com/test/ConcurrentHashMapTest.java
+++ src/com/test/ConcurrentHashMapTest.java
@@ -4,6 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * 在put时当有hash碰撞则加锁,在对应hash中的链表添加数据
+ * 如果在添加是遇到forwardNode则证明该map正在扩容,帮助map扩容
+ * 扩容时,把底层数组分成一个个hash步长,每个线程对应处理一个步长,
+ * 如果(sc - 2) != resizeStamp(n) << RESIZE_STAMP_SHIFT 说明扩容完成
+ */
 public class ConcurrentHashMapTest {
 
    static ConcurrentHashMap<String,Object> map = new ConcurrentHashMap(1);
